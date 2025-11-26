babfaad512cef1029223428072efb1f6aadb45b3
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-26 01:10:30 +0000

    Update manage.py

--- alx_travel_app/manage.py
+++ alx_travel_app/manage.py
@@ -1,1 +1,15 @@
 Django default
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError("Django not installed") from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
